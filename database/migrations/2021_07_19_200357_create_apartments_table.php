5f2d5dc808117d27fe6308a8324ca5371d8eb054
StefanoCeli <stefanopsn91@gmail.com>
2021-07-20 15:09:32 +0200

    migration

---
+++ database/migrations/2021_07_19_200357_create_apartments_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApartmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartments', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',50);
+            $table->boolean('visible');
+            $table->string('image');
+            $table->tinyInteger('guest_number');
+            $table->tinyInteger('rooms');
+            $table->float('nightly_price',6,2);
+            $table->tinyInteger('bed_number');
+            $table->tinyInteger('bathroom_number');
+            $table->string('square_meters',4);
+            $table->text('description');
+            $table->string('country',50);
+            $table->string('city',50);
+            $table->string('address',50);
+            $table->string('postal_code',10);
+            $table->string('longitude',50);
+            $table->string('latitude',50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apartments');
+    }
+}
---
+++ database/migrations/2021_07_20_082756_create_requests_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',50);
+            $table->string('surname',50);
+            $table->string('email',100);
+            $table->string('title',50);
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('requests');
+    }
+}
