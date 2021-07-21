e761395c53f5dab4b48853107bbc95fd4f9afafa
StefanoCeli <stefanopsn91@gmail.com>
2021-07-21 09:57:20 +0200

    end model and to do seeder

--- app/Apartment.php
+++ app/Apartment.php
@@ -9,4 +9,32 @@ class Apartment extends Model
     public function user(){
         return $this->belongsTo('App\User');
     }
+
+    public function apartment_images(){
+        return $this->hasMany('App\Apartment_image');
+    }
+
+    public function requests(){
+        return $this->hasMany('App\Request');
+    }
+
+    public function reviews(){
+        return $this->hasMany('App\Review');
+    }
+    
+    public function categories(){
+        return $this->hasMany('App\Category');
+    }
+
+    public function views(){
+        return $this->hasMany('App\View');
+    }
+
+    public function services(){
+        return $this->belongsToMany('App\Service');
+    }
+
+    public function sponsors(){
+        return $this->belongsToMany('App\Sponsor');
+    }
 }
--- app/Apartment_image.php
+++ app/Apartment_image.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Apartment_image extends Model
 {
-    //
+    public function apartment(){
+        return $this->belongsTo('App\Apartment');
+    }
 }
